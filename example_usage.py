5aa612bc2281bdcaa6e8a7cc20e1dc2a32279d3e
Mert Cobanov <mertcobanov@gmail.com>
2024-08-29 11:17:39 -0700

    chore: Update prompt text for CLIP Text Encode nodes

--- example_usage.py
+++ example_usage.py
@@ -8,16 +8,16 @@ import websocket
 import utils
 
 # Client configuration
-server_address: str = "127.0.0.1:8188"
+server_address: str = "dgx4:8188"
 client_id: str = "alex"  # for random -> str(uuid.uuid4())
-prompt_path = "./workflow_api.json"
+prompt_path = "./dgx_workflow.json"
 output_path = "output"
 
 prompt = utils.load_prompt(prompt_path)
 
 # Modify the workflow
-prompt["6"]["inputs"]["text"] = "masterpiece best quality painting"
-prompt["7"]["inputs"][
+prompt["39"]["inputs"]["text"] = "masterpiece best quality painting"
+prompt["40"]["inputs"][
     "text"
 ] = "naked, nude, nsfw, erotic, sexy"  # Negative text if needed
 seed = random.randint(0, 1000000)
@@ -31,13 +31,13 @@ ws.connect(f"ws://{server_address}/ws?clientId={client_id}")
 # Get the images
 images = utils.get_images(ws, prompt, server_address, client_id)
 
-# # Save the images optional
-# for node_id in images:
-#     for image_data in images[node_id]:
-#         formatted_time = datetime.now().strftime("%y%m%d_%H%M")
-#         utils.save_image(
-#             image_data, f"{output_path}/{client_id}_{formatted_time}_s{seed}.png"
-#         )
+# Save the images optional
+for node_id in images:
+    for image_data in images[node_id]:
+        formatted_time = datetime.now().strftime("%y%m%d_%H%M")
+        utils.save_image(
+            image_data, f"{output_path}/{client_id}_{formatted_time}_s{seed}.png"
+        )
 
 
 # Close the WebSocket connection
